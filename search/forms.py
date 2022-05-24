406df8a9616ac380caa2a8cf3b7a3568752c75f7
unknown <arthuromgnh@gmail.com>
2022-05-25 01:06:51 +0300

    Changed index.html

--- search/forms.py
+++ search/forms.py
@@ -2,4 +2,16 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search_request = forms.CharField(max_length=255, required=False)
+    search_request = forms.CharField(
+        max_length=255,
+        required=False,
+        label='',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'rounded',
+                'type': 'search',
+                'placeholder': 'Search',
+
+            }
+        )
+    )
